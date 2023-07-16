e09065cb1cfbf354d6b0e909ccf49fa2ca2f6cfe
Ridwan <ridwanul.alam96@gmail.com>
2023-07-16 23:36:28 +0600

    finished added book list

---
+++ src/components/List/SingleBookList.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable no-unsafe-optional-chaining */
+import {
+	Card,
+
+	Text,
+	Badge,
+	Group,
+	Center,
+	Avatar,
+	createStyles,
+	rem,
+	Box,
+} from '@mantine/core';
+import { Link } from 'react-router-dom';
+
+const useStyles = createStyles((theme) => ({
+	card: {
+		position: 'relative',
+		backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+	},
+
+	rating: {
+		position: 'absolute',
+		top: theme.spacing.xs,
+		right: rem(12),
+		pointerEvents: 'none',
+	},
+
+	title: {
+		display: 'block',
+		marginTop: theme.spacing.md,
+		marginBottom: rem(5),
+		color: "white",
+		// textDecoration: '0',
+	},
+
+	action: {
+		// backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+		// ...theme.fn.hover({
+		// 	backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+		// }),
+	},
+
+	footer: {
+		marginTop: theme.spacing.xs,
+	},
+}));
+
+
+
+export function SingleBookList({ book }: any) {
+	const { classes, cx, } = useStyles();
+	const { title, author, publication, id, genre } = book?.bookId;
+
+
+	return (
+		<>
+			{
+				title ? <Card withBorder radius="md" className={cx(classes.card)} >
+					<Link to={`/book/${id as string}`}>	<Card.Section mt={-20}>
+						<Box sx={{ height: 100, width: '100%', background: '#0B7285', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+							<Text className={classes.title} fw={700} >
+								{title}
+							</Text>
+						</Box>
+					</Card.Section>
+					</Link>
+
+					<Badge className={classes.rating} variant="gradient" gradient={{ from: 'yellow', to: 'red' }}>
+						{genre}
+					</Badge>
+
+
+
+					{/* <Text fz="sm" color="dimmed" lineClamp={2}>
+				{description}
+			</Text> */}
+
+					<Group className={classes.footer}>
+						<Center>
+							<Avatar size={24} radius="xl" color='cyan' mr="xs" >
+								{author?.split(' ')[0].slice(0, 1) + author?.split(' ')[1]?.slice(0, 1)}
+							</Avatar>
+							<Text fz="sm" inline>
+								{author}
+							</Text>
+
+						</Center>
+						<Text fz="sm" inline>
+							Publication: {publication}
+						</Text>
+
+						{/* <Group spacing={8} mr={0}>
+					<ActionIcon className={classes.action}>
+						<IconHeart size="1rem" color={theme.colors.red[6]} />
+					</ActionIcon>
+					<ActionIcon className={classes.action}>
+						<IconBookmark size="1rem" color={theme.colors.yellow[7]} />
+					</ActionIcon>
+
+				</Group> */}
+					</Group>
+				</Card > : null
+			}
+		</>
+	);
+}
---
+++ src/pages/WatchList.tsx
@@ -0,0 +1,159 @@
+import { Button, Container, Grid, Modal, ScrollArea, Text, Tooltip } from '@mantine/core';
+import { SearchArea } from '../components/Home/SearchArea';
+import Filters from '../components/Home/Filters';
+import BookLists from '../components/Home/BookLists';
+import { useDisclosure } from '@mantine/hooks';
+import AddBook from '../components/Home/AddBook';
+import { createStyles, Title, rem } from '@mantine/core';
+import { Dots } from '../atoms/Dots';
+import { useNavigate } from 'react-router-dom';
+import { useFeaturedBookQuery } from '../redux/features/books/bookApi';
+import { useGetWishlistQuery } from '../redux/features/personalList/listApi';
+
+const WatchList = () => {
+	const { classes } = useStyles();
+	const navigate = useNavigate();
+	const [opened, { open, close }] = useDisclosure(false);
+	const { data: allWishlist, isLoading } = useGetWishlistQuery({});
+
+
+	return (
+		<Container size="lg" px="xs">
+			<Grid>
+				{/* TOP */}
+				<Modal opened={opened} onClose={close} title="Add New Book" centered>
+					<AddBook />
+				</Modal>
+
+
+				{/* MAIN PART */}
+				<Grid.Col span={12}>
+					<Grid align='end' justify='center' my={10} >
+						<Container className={classes.wrapper} size={1400}>
+							<Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+							<Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+							<Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+							<Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+
+							<div className={classes.inner}>
+								<Title className={classes.title}>
+									<Text component="span" className={classes.highlight} inherit>
+										My Watch List
+									</Text>
+								</Title>
+
+
+
+								<div className={classes.controls}>
+									<Button className={classes.control} onClick={() => navigate("/all")} size="lg" variant="default" color="gray">
+										See all books
+									</Button>
+									<Button onClick={open} className={classes.control} size="lg">
+										Add New Book
+									</Button>
+								</div>
+							</div>
+						</Container>
+					</Grid>
+					<ScrollArea h={{ md: 400, lg: 450 }} type="never" >
+						<Text fs={'revert'} sx={{ fontSize: 25, textAlign: 'center' }} fw={700} my={30} inline>
+							Watch List
+						</Text>
+						<BookLists type='list' books={allWishlist?.data} isLoading={isLoading} />
+					</ScrollArea>
+				</Grid.Col>
+
+			</Grid >
+		</Container >
+	);
+};
+
+export default WatchList;
+
+
+
+const useStyles = createStyles((theme) => ({
+	wrapper: {
+		position: 'relative',
+		paddingTop: rem(120),
+		paddingBottom: rem(80),
+
+		[theme.fn.smallerThan('sm')]: {
+			paddingTop: rem(80),
+			paddingBottom: rem(60),
+		},
+	},
+
+	inner: {
+		position: 'relative',
+		zIndex: 1,
+	},
+
+	dots: {
+		position: 'absolute',
+		color: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+
+		[theme.fn.smallerThan('sm')]: {
+			display: 'none',
+		},
+	},
+
+	dotsLeft: {
+		left: 0,
+		top: 0,
+	},
+
+	title: {
+		textAlign: 'center',
+		fontWeight: 800,
+		fontSize: rem(40),
+		letterSpacing: -1,
+		color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+		marginBottom: theme.spacing.xs,
+		fontFamily: `Greycliff CF, ${theme.fontFamily!}`,
+
+		[theme.fn.smallerThan('xs')]: {
+			fontSize: rem(28),
+			textAlign: 'left',
+		},
+	},
+
+	highlight: {
+		color: theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6],
+	},
+
+	description: {
+		textAlign: 'center',
+
+		[theme.fn.smallerThan('xs')]: {
+			textAlign: 'left',
+			fontSize: theme.fontSizes.md,
+		},
+	},
+
+	controls: {
+		marginTop: theme.spacing.lg,
+		display: 'flex',
+		justifyContent: 'center',
+
+		[theme.fn.smallerThan('xs')]: {
+			flexDirection: 'column',
+		},
+	},
+
+	control: {
+		'&:not(:first-of-type)': {
+			marginLeft: theme.spacing.md,
+		},
+
+		[theme.fn.smallerThan('xs')]: {
+			height: rem(42),
+			fontSize: theme.fontSizes.md,
+
+			'&:not(:first-of-type)': {
+				marginTop: theme.spacing.md,
+				marginLeft: 0,
+			},
+		},
+	},
+}));
