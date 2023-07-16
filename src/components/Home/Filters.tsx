5682b6576d42aa5916d88a22733f2bab0dc47d5e
Ridwan <ridwanul.alam96@gmail.com>
2023-07-16 19:44:27 +0600

    done filter and search

--- src/components/Home/Filters.tsx
+++ src/components/Home/Filters.tsx
@@ -2,22 +2,26 @@ import { Box, NativeSelect, Text } from '@mantine/core';
 import { useState } from 'react';
 import { Group } from '@mantine/core';
 import { YearPicker } from '@mantine/dates';
+import { useToggle } from '@mantine/hooks';
+import { useGenreListQuery } from '../../redux/features/books/bookApi';
 
-export default function Filters() {
-	const [value, setValue] = useState<Date | null>(null);
+export default function Filters({ yearValue, setYearValue, setGenreValue }) {
+
+	const { data, isLoading } = useGenreListQuery({})
 
 	return (
 		<Box p={20}>
 			<NativeSelect
 				mt={10}
-				data={['React', 'Vue', 'Angular', 'Svelte']}
+				data={isLoading ? [''] : ['', ...data?.data]}
 				label="Filter by genre"
 				// description="Filter by genre"
 				withAsterisk
+				onChange={(e) => setGenreValue(e.target.value)}
 			/>
 			<Group mt={20} >
 				<Text fz='xs'>Filter By Publication Year</Text>
-				<YearPicker allowDeselect mt={-10} size='xs' value={value} onChange={setValue} />
+				<YearPicker allowDeselect mt={-10} maxDate={new Date()} size='xs' value={yearValue} onChange={setYearValue} />
 			</Group>
 		</Box>
 
--- src/components/Home/SearchArea.tsx
+++ src/components/Home/SearchArea.tsx
@@ -1,23 +1,15 @@
-import { TextInput, TextInputProps, ActionIcon, useMantineTheme } from '@mantine/core';
+import { TextInput, TextInputProps, ActionIcon, useMantineTheme, Loader } from '@mantine/core';
 import { IconSearch, IconArrowRight, IconArrowLeft } from '@tabler/icons-react';
 
 export function SearchArea(props: TextInputProps) {
-	const theme = useMantineTheme();
+
 
 	return (
 		<TextInput
 			icon={<IconSearch size="1.1rem" stroke={1.5} />}
 			radius="xl"
 			size="md"
-			rightSection={
-				<ActionIcon size={32} radius="xl" color={theme.primaryColor} variant="filled">
-					{theme.dir === 'ltr' ? (
-						<IconArrowRight size="1.1rem" stroke={1.5} />
-					) : (
-						<IconArrowLeft size="1.1rem" stroke={1.5} />
-					)}
-				</ActionIcon>
-			}
+
 			placeholder="Search Book..."
 			rightSectionWidth={42}
 			{...props}
