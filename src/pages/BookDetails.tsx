c88bdc4431c1fed36d4edf85ef61c177fc6c91fc
Ridwan <ridwanul.alam96@gmail.com>
2023-07-16 21:00:30 +0600

    bookdetails page done

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -17,15 +17,29 @@ import {
 	Textarea,
 	Divider,
 	Button,
+	Tooltip,
 } from '@mantine/core';
 import { IconBookmark, IconHeart } from '@tabler/icons-react';
 import { reviews } from '../components/BookDetails/_mockData';
 import Review from '../components/BookDetails/Review';
 import IReview from '../types/review';
 import { hasLength, useForm } from '@mantine/form';
+import { useAddReviewMutation, useGetReviewsQuery, useSingleBookQuery } from '../redux/features/books/bookApi';
+import { useParams } from 'react-router-dom';
+import { BookType } from '../types/book';
+import { useAppSelector } from '../redux/hook';
+import { notifications } from '@mantine/notifications';
+import { IconX } from '@tabler/icons-react';
 const useStyles = createStyles(() => ({
 }));
 const BookDetails = () => {
+	const { id: bookId } = useParams();
+	const { data } = useSingleBookQuery(bookId as string);
+	const { data: allreviews } = useGetReviewsQuery(bookId as string);
+	const { user } = useAppSelector(state => state.auth)
+	const [addReview, { isLoading }] = useAddReviewMutation();
+
+
 	const form = useForm({
 		initialValues: {
 			review: '',
@@ -36,21 +50,8 @@ const BookDetails = () => {
 		},
 	});
 	// props: BookType
-	const demo = {
-		image:
-			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrBstXAmkzVK-Ze6Lg_gZMVl57-7Oyvpw6QA&usqp=CAU",
-		link: "id",
-		title: "Resident Evil Village review",
-		rating: "outstanding",
-		description:
-			"Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires. Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires.",
-		author: {
-			name: "Bill Wormeater",
-			image:
-				"https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80",
-		},
-	}
-	const { rating, title, author, description, image } = demo;
+
+	const { genre, title, author, publication, createdAt, id, addedBy }: BookType = data?.data || {};
 	const { theme } = useStyles();
 	return (
 		<Container size={'md'}>
@@ -58,7 +59,7 @@ const BookDetails = () => {
 				<Grid.Col span={4}>
 					<Card.Section sx={{ display: "flex", justifyContent: "center" }}>
 
-						<Image src={image} height={250} width={200} />
+						<Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrBstXAmkzVK-Ze6Lg_gZMVl57-7Oyvpw6QA&usqp=CAU" height={250} width={200} />
 
 					</Card.Section>
 				</Grid.Col>
@@ -67,42 +68,86 @@ const BookDetails = () => {
 					<Flex direction={'row'} align={'center'}>
 						<Avatar size={24} radius="xl" mr="xs" color='cyan' >
 
-							{author.name.split(' ')[0].slice(0, 1) + author.name?.split(' ')[1]?.slice(0, 1)}
+							{author?.split(' ')[0].slice(0, 1) + author?.split(' ')[1]?.slice(0, 1)}
 						</Avatar>
-						<Text fz="sm" inline>
-							{author.name}
+						<Text fz="lg" inline>
+							{author}
 						</Text>
 						<Badge ml={10} variant="gradient" gradient={{ from: 'yellow', to: 'red' }}>
-							{rating}
+							{genre}
 						</Badge>
 					</Flex>
-					<Text fw={500} component="h2" >
+					<Text fw={500} fz="xl" my={20} >
 						{title}
+						<Text fz="xs"   >
+							Published On: {publication}
+							<br />
+
+						</Text>
 					</Text>
 
+
 					<Text fz="xs"  >
-						{description}
+						Added By: {addedBy?.fullName}
+						<br />
+						Email ID: {addedBy?.email}
+
 					</Text>
 				</Grid.Col>
 			</Grid>
 			<Group position="right" >
 				<Group spacing={8} mr={0} position='right'>
-					<ActionIcon >
 
+					<ActionIcon >
+						<Tooltip label="Add to wishlist">
+
+							<IconHeart size="1rem" color={theme.colors.red[6]} />
+						</Tooltip>
 					</ActionIcon>
 					<ActionIcon >
-						<IconHeart size="1rem" color={theme.colors.red[6]} />
-					</ActionIcon>
-					<ActionIcon >
-						<IconBookmark size="1rem" color={theme.colors.yellow[7]} />
+						<Tooltip label="Add to currently reading">
+
+							<IconBookmark size="1rem" color={theme.colors.yellow[7]} />
+						</Tooltip>
 					</ActionIcon>
 
 				</Group>
 			</Group>
 
 			<Container size={'xs'} mb={20}>
-				<form onSubmit={form.onSubmit((values) => {
-					console.log('values: ', values);
+				<form onSubmit={form.onSubmit(async (values): void => {
+					const formData = {
+
+						review: values.review,
+						bookId: bookId,
+						reviewerId: user?.id
+					}
+
+					const res = await addReview(formData as {
+						review: string;
+						bookId: string;
+						reviewerId: string;
+					});
+					notifications.show({
+						id: 'success-login',
+						withCloseButton: true,
+						onClose: () => console.log('unmounted'),
+						onOpen: () => console.log('mounted'),
+						autoClose: 3000,
+						title: res?.data?.statusCode === 200 ? "Review Added" : "Operation Failed",
+						message: res?.data?.statusCode === 200 ? res.data?.message : res?.error?.data.message,
+						color: res?.data?.statusCode === 200 ? 'cyan' : 'red',
+						icon: <IconX />,
+						className: 'my-notification-class',
+						style: { backgroundColor: '' },
+						sx: { backgroundColor: 'white' },
+						loading: false,
+					});
+
+
+					res?.data?.statusCode === 200 && form.reset();
+					console.log('formData: ', formData);
+
 
 				})}>
 					<Grid align='end'>
@@ -116,14 +161,14 @@ const BookDetails = () => {
 							/>
 						</Grid.Col>
 						<Grid.Col span={2}>
-							<Button type='submit'>
+							<Button disabled={isLoading} type='submit'>
 								Save
 							</Button>
 						</Grid.Col>
 					</Grid>
 					<Divider my={10} />
 					{
-						reviews.map((review: IReview) => <Review postedAt={review.postedAt} body={review.body} author={review.author} />)
+						allreviews?.data?.map((review: IReview) => <Review createdAt={review.createdAt} review={review.review} author={review.reviewerId.fullName} />)
 					}
 
 				</form>
@@ -133,3 +178,18 @@ const BookDetails = () => {
 };
 
 export default BookDetails;
+
+const demo = {
+	image:
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrBstXAmkzVK-Ze6Lg_gZMVl57-7Oyvpw6QA&usqp=CAU",
+	link: "id",
+	title: "Resident Evil Village review",
+	rating: "outstanding",
+	description:
+		"Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires. Resident Evil Village is a direct sequel to 2017’s Resident Evil 7, but takes a very different direction to its predecessor, namely the fact that this time round instead of fighting against various mutated zombies, you’re now dealing with more occult enemies like werewolves and vampires.",
+	author: {
+		name: "Bill Wormeater",
+		image:
+			"https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80",
+	},
+}
